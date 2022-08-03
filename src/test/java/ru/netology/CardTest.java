3cf674a3b2a132aafa558ce3829cf2e02696efc6
LokiTheDark666 <khcr.loki@mail.ru>
2022-08-03 19:32:01 +0300

    add test

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -1,15 +1,9 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -23,13 +17,12 @@ import static com.codeborne.selenide.Selenide.open;
 public class CardTest {
 
 
-
     @BeforeEach
-        void openUrl() {
-            open("http://localhost:9999");
-        }
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    void openUrl() {
+        open("http://localhost:9999");
+    }
 
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
 
     @Test
