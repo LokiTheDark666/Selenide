99de4569a5a85d6ca7687d3a37ce57af7f9c272c
LokiTheDark666 <khcr.loki@mail.ru>
2022-08-09 19:33:04 +0300

    add test

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -1,32 +1,32 @@
 package ru.netology;
 
+
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.exist;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class CardTest {
+
+    private Meeting meeting = DataGenerator.Registration.generate();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
 
     @BeforeEach
     void openUrl() {
         open("http://localhost:9999");
     }
-
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
 
 
     @Test
@@ -35,18 +35,24 @@ public class CardTest {
         Configuration.holdBrowserOpen = true;
 
         SelenideElement form = $(".form");
-        form.$("[data-test-id=city] input").setValue("Москва");
+        form.$("[data-test-id=city] input").setValue(meeting.getCity());
         form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
-        form.$("[data-test-id=date] input").setValue(formatter.format(LocalDate.now().plusDays(3)));
-        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич-Пупкин");
-        form.$("[data-test-id=phone] input").setValue("+78889996565");
+        form.$("[data-test-id=date] input").setValue(formatter.format(meeting.getDateFirstMeeting()));
+        form.$("[data-test-id=name] input").setValue(meeting.getFirstName() + " " + meeting.getLastName());
+        form.$("[data-test-id=phone] input").setValue("+79996664545");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $("[data-test-id='notification']").shouldBe(exist, Duration.ofSeconds(15));
-        SelenideElement selenideElement = $(".notification__content")
-                .shouldHave(Condition.text(("Встреча успешно забронирована на ") + formatter.format(LocalDate.now().plusDays(3))), Duration.ofSeconds(15)).shouldBe(Condition.visible);
 
+
+        form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(formatter.format(meeting.getDateSecondMeeting()));
+        form.$(".button").click();
+        $("[data-test-id=replan-notification]").shouldBe(exist);
+        $$(".button").find(exactText("Перепланировать")).click();
 
     }
 
+
 }
+
+
